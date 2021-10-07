52fa1cd1227ea22d21e53b0b80db1fd5d8a1686f
Pixmi <Pixmi@users.noreply.github.com>
2021-10-07 14:01:43 +0800

    偵測OS

--- script/default.js
+++ script/default.js
@@ -1,4 +1,5 @@
 const md = new MobileDetect(window.navigator.userAgent);
+/*
 console.log( md.mobile() );          // 'Sony'
 console.log( md.phone() );           // 'Sony'
 console.log( md.tablet() );          // null
@@ -9,3 +10,18 @@ console.log( md.is('bot') );         // false
 console.log( md.version('Webkit') );         // 534.3
 console.log( md.versionStr('Build') );       // '4.1.A.0.562'
 console.log( md.match('playstation|xbox') ); // false
+*/
+function deviceOS() {
+    if (md.match('Windows NT') && md.version('Windows NT') > 0) {
+        return `Windows ${md.version('Windows NT')}`;
+    } else
+    if (md.match('Android') && md.version('Android') > 0) {
+        return `Android OS ${md.version('Android')}`;
+    } else
+    if ((md.match('iOS') || md.match('iPad')) && md.version('iOS') > 0) {
+        return `iOS ${md.version('iOS')}`;
+    } else {
+        return '-';
+    }
+}
+console.log(deviceOS());
