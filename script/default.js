16347a117c9a515c38408a18111d7aa881687f04
Pixmi <Pixmi@users.noreply.github.com>
2021-10-07 16:32:57 +0800

    移除

--- script/default.js
+++ script/default.js
@@ -1,16 +1,5 @@
 const md = new MobileDetect(window.navigator.userAgent);
-/*
-console.log( md.mobile() );          // 'Sony'
-console.log( md.phone() );           // 'Sony'
-console.log( md.tablet() );          // null
-console.log( md.userAgent() );       // 'Safari'
-console.log( md.os() );              // 'AndroidOS'
-console.log( md.is('iPhone') );      // false
-console.log( md.is('bot') );         // false
-console.log( md.version('Webkit') );         // 534.3
-console.log( md.versionStr('Build') );       // '4.1.A.0.562'
-console.log( md.match('playstation|xbox') ); // false
-*/
+
 function deviceOS() {
     let os;
     if (md.match('Windows NT') && md.version('Windows NT') > 0) {
